39048466797b023e29016098e336ecc9f8b14e87
unxxy <xys534393426@163.com>
2024-09-12 22:10:54 +0800

    ✨ feat: 文件的增删改

--- src/components/FileNameList/FileNameItem.tsx
+++ src/components/FileNameList/FileNameItem.tsx
@@ -1,18 +1,52 @@
 import classnames from 'classnames'
 import React, { useState, useRef, useEffect } from 'react'
-
+import { Popconfirm } from 'antd'
 import styles from './index.module.scss'
 
 export interface FileNameItemProps {
   value: string
   activated: boolean
+  onEditComplete: (name: string) => void
+  creating: boolean // 新增
+  readonly: boolean // 只读文件不能修改和删除
   onClick: () => void
+  onRemove: () => void
 }
 
 export const FileNameItem: React.FC<FileNameItemProps> = props => {
-  const { value, activated: activated = false, onClick } = props
+  const {
+    value,
+    activated = false,
+    creating, // 是否新增
+    readonly,
+    onClick,
+    onEditComplete,
+    onRemove,
+  } = props
 
   const [name, setName] = useState(value)
+
+  const [editing, setEditing] = useState(creating)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  // 双击编辑文件名称
+  const handleDoubleClick = () => {
+    setEditing(true)
+    setTimeout(() => {
+      inputRef?.current?.focus()
+    }, 0)
+  }
+
+  const handleInputBlur = () => {
+    setEditing(false)
+    onEditComplete(name)
+  }
+
+  useEffect(() => {
+    if (creating) {
+      inputRef.current?.focus()
+    }
+  }, [creating])
 
   return (
     <div
@@ -22,7 +56,39 @@ export const FileNameItem: React.FC<FileNameItemProps> = props => {
       )}
       onClick={onClick}
     >
-      <span>{name}</span>
+      {editing ? (
+        <input
+          ref={inputRef}
+          className={styles['tabs-item-input']}
+          value={name}
+          onBlur={handleInputBlur}
+          onChange={e => setName(e.target.value)}
+        />
+      ) : (
+        <>
+          <span onDoubleClick={!readonly ? handleDoubleClick : () => {}}>
+            {name}
+          </span>
+          {!readonly ? (
+            <Popconfirm
+              title="确认删除该文件吗？"
+              okText="确定"
+              cancelText="取消"
+              onConfirm={e => {
+                e?.stopPropagation()
+                onRemove()
+              }}
+            >
+              <span style={{ marginLeft: 5, display: 'flex' }}>
+                <svg width="12" height="12" viewBox="0 0 24 24">
+                  <line stroke="#999" x1="18" y1="6" x2="6" y2="18"></line>
+                  <line stroke="#999" x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
+              </span>
+            </Popconfirm>
+          ) : null}
+        </>
+      )}
     </div>
   )
 }
--- src/components/FileNameList/index.tsx
+++ src/components/FileNameList/index.tsx
@@ -2,13 +2,18 @@ import { PlaygroundContext } from '@/Playground/PlaygroundContext'
 import { useContext, useEffect, useState } from 'react'
 import { FileNameItem } from './FileNameItem'
 import styles from './index.module.scss'
+import {
+  APP_COMPONENT_FILE_NAME,
+  ENTRY_FILE_NAME,
+  IMPORT_MAP_FILE_NAME,
+} from '@/Playground/files'
 
 export default function FileNameList() {
   const {
     files,
-    // removeFile,
-    // addFile,
-    // updateFileName,
+    removeFile,
+    addFile,
+    updateFileName,
     selectedFileName,
     setSelectedFileName,
   } = useContext(PlaygroundContext)
@@ -19,16 +24,56 @@ export default function FileNameList() {
     setTabs(Object.keys(files))
   }, [files])
 
+  // 修改名称
+  const handleEditComplete = (name: string, prevName: string) => {
+    updateFileName(prevName, name)
+    setSelectedFileName(name)
+    setCreating(false)
+  }
+
+  const [creating, setCreating] = useState(false)
+
+  // 新增文件
+  const addTab = () => {
+    const newFileName = 'Comp' + Math.random().toString().slice(2, 8) + '.tsx'
+    addFile(newFileName)
+    setSelectedFileName(newFileName)
+    setCreating(true)
+  }
+
+  // 删除文件
+  const handleRemove = (name: string) => {
+    // e.stopPropagation() // 防止触发 tab 切换
+    removeFile(name)
+    setSelectedFileName(ENTRY_FILE_NAME)
+  }
+
+  // 只读文件不能被删除和编辑
+  const readonlyFileNames = [
+    ENTRY_FILE_NAME,
+    IMPORT_MAP_FILE_NAME,
+    APP_COMPONENT_FILE_NAME,
+  ]
+
   return (
     <div className={styles.tabs}>
       {tabs.map((item, index) => (
         <FileNameItem
           key={item + index}
           value={item}
+          creating={creating}
           activated={selectedFileName === item}
+          readonly={readonlyFileNames.includes(item)}
+          onEditComplete={(name: string) =>
+            handleEditComplete(name, selectedFileName)
+          }
           onClick={() => setSelectedFileName(item)}
+          onRemove={() => handleRemove(item)}
         />
       ))}
+      <div className={styles.add} onClick={addTab}>
+        +
+      </div>
     </div>
   )
 }
