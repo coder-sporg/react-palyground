62a5c9d5996d7b4b1a78a3145fde0fe8348417e5
unxxy <xys534393426@163.com>
2024-09-13 09:52:39 +0800

    ✨ feat: 代码下载

--- src/Playground/utils.ts
+++ src/Playground/utils.ts
@@ -1,4 +1,7 @@
 import { strFromU8, strToU8, unzlibSync, zlibSync } from 'fflate'
+import JSZip from 'jszip'
+import saveAs from 'file-saver'
+import { Files } from './PlaygroundContext'
 
 export const fileName2Language = (name: string) => {
   const suffix = name.split('.').pop() || ''
@@ -27,4 +30,16 @@ export function unCompress(base64: string): string {
   const buffer = strToU8(binary, true)
   const unzipped = unzlibSync(buffer)
   return strFromU8(unzipped)
+}
+
+// 代码下载和压缩
+export async function downloadFiles(files: Files) {
+  const zip = new JSZip()
+
+  Object.keys(files).forEach(name => {
+    zip.file(name, files[name].value)
+  })
+
+  const blob = await zip.generateAsync({ type: 'blob' })
+  saveAs(blob, `code${Math.random().toString().slice(2, 8)}.zip`)
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import { MoonOutlined, ShareAltOutlined, SunOutlined } from '@ant-design/icons'
+import {
+  DownloadOutlined,
+  MoonOutlined,
+  ShareAltOutlined,
+  SunOutlined,
+} from '@ant-design/icons'
 import styles from './index.module.scss'
 import logoSvg from '@/assets/react.svg'
 import { useContext } from 'react'
 import { PlaygroundContext } from '@/Playground/PlaygroundContext'
 import { message } from 'antd'
 import copy from 'copy-to-clipboard'
+import { downloadFiles } from '@/Playground/utils'
 
 export default function Header() {
-  const { theme, setTheme } = useContext(PlaygroundContext)
+  const { theme, setTheme, files } = useContext(PlaygroundContext)
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -34,6 +40,13 @@ export default function Header() {
           onClick={() => {
             copy(window.location.href)
             message.success('分享链接已复制!')
+          }}
+        />
+        <DownloadOutlined
+          style={{ marginLeft: '10px' }}
+          onClick={async () => {
+            await downloadFiles(files)
+            message.success('下载完成!')
           }}
         />
       </div>
